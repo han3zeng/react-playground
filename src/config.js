f7df801a4193007634348a320a8d45c102a29678
Han <allen0346@gmail.com>
2021-08-23 14:53:53 +0800

    Adjust: config based on env

--- src/config.js
+++ src/config.js
@@ -3,8 +3,9 @@ const config = {
   githubClientId: '3f488128d8bad7618828',
   googleCleintId: '137621437327-lp61tg8a7ks6t4hbs33mvlri11t8fo0v.apps.googleusercontent.com',
   redirectUrl: 'http://localhost:3000/login-callback',
+  origin: process.env.NODE_ENV === 'production' ? 'https://react-playground-7kgn6zbeya-uc.a.run.app/' : 'http://localhost:3000',
   authServerOrigin: process.env.NODE_ENV === 'production' ? 'https://authorization-server-7kgn6zbeya-uc.a.run.app' : 'http://localhost:8080',
-  resourceServerOrigin: 'http://localhost:8081',
+  resourceServerOrigin: process.env.NODE_ENV === 'production' ? 'https://resource-server-7kgn6zbeya-uc.a.run.app' : 'http://localhost:8081'
 };
 
 const breakpoints = {
