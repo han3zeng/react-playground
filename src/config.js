d17e624769c9f0ffc250115c52d519358c9a0cce
Han <allen0346@gmail.com>
2021-08-23 15:45:17 +0800

    Bug: remove salsh from tail of originProd

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const config = {
   githubClientId: process.env.NODE_ENV === 'production' ? 'bc32cf5b344432cf78cb' : '3f488128d8bad7618828',
   googleCleintId: '137621437327-lp61tg8a7ks6t4hbs33mvlri11t8fo0v.apps.googleusercontent.com',
   redirectPath: 'login-callback',
-  origin: process.env.NODE_ENV === 'production' ? 'https://react-playground-7kgn6zbeya-uc.a.run.app/' : 'http://localhost:3000',
+  origin: process.env.NODE_ENV === 'production' ? 'https://react-playground-7kgn6zbeya-uc.a.run.app' : 'http://localhost:3000',
   authServerOrigin: process.env.NODE_ENV === 'production' ? 'https://authorization-server-7kgn6zbeya-uc.a.run.app' : 'http://localhost:8080',
   resourceServerOrigin: process.env.NODE_ENV === 'production' ? 'https://resource-server-7kgn6zbeya-uc.a.run.app' : 'http://localhost:8081'
 };
