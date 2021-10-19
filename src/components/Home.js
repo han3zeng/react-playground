33e5288974f32edf1f013004c2299ec71b21d4eb
Han <allen0346@gmail.com>
2021-10-19 16:42:18 +0800

    Adjust: content of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,11 +11,18 @@ const Container = styled.div`
 function Home() {
   return (
     <Container>
-      <h2>Overview</h2>
+      <h3>Overview</h3>
       <p>This is the Han's playground to practice web development related subjects.</p>
       <p>Currently, I have been working on personal blog, authorization and authentication system.</p>
-      <h2>How to use the Site?</h2>
+      <p>I am still working on the project. So there are only few features available for this Blog service.</p>
+      <h3>How to use the site?</h3>
       <p>Sign up or Sign in first, then you can have your own blog.</p>
+      <h3>Currnet available Features</h3>
+      <ul>
+        <li>Create account</li>
+        <li>Publish story</li>
+        <li>See all your story titles in story list</li>
+      </ul>
     </Container>
   );
 }
